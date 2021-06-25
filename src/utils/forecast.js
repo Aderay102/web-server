b65e8706021b1dcb26403ba86b9d4e917a19c508
Aderay102 <Aderay102@gmail.com>
2021-06-25 12:07:03 +0700

    Add new data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longtitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, "It's currently " + body.current.weather_descriptions[0] + " degress out. It feels like " + body.current.feelslike + " degress out.")
+            callback(undefined, body.current.weather_descriptions[0] + ". It's currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degress out. The humidity is " + body.current.humidity + "%.")
         }
     })
 }
